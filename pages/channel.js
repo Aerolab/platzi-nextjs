2212ad8d94d1556c92848863c8875753d77d3f0d
Roberto Gonzalez <roberto@aerolab.co>
2018-07-09 02:08:04 -0300

    4.Link

--- pages/channel.js
+++ pages/channel.js
@@ -1,30 +1,10 @@
-import 'isomorphic-fetch'
-import Link from 'next/link'
-
 export default class extends React.Component {
 
-  static async getInitialProps() {
-    let req = await fetch('https://api.audioboom.com/channels/recommended')
-    let { body: channels } = await req.json()
-    return { channels }
-  }
-
   render() {
-    const { channels } = this.props
-
     return <div>
       <header>Podcasts</header>
 
-      <div className="channels">
-        { channels.map((channel) => (
-          <Link href={`/channel?id=${channel.id}`} prefetch key={ channel.id }>
-            <a className="channel">
-              <img src={ channel.urls.logo_image.original } alt=""/>
-              <h2>{ channel.title }</h2>
-            </a>
-          </Link>
-        )) }
-      </div>
+      <h1>Canal</h1>
 
       <style jsx>{`
         header {
