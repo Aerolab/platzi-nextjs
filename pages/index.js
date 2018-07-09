14f982f412ab719039dc983f75db6c18df6f4def
Roberto Gonzalez <roberto@aerolab.co>
2018-07-09 02:03:00 -0300

    2.StyledJSX

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,29 @@ export default class extends React.Component {
   render() {
     return <div>
       <h1>¡Hola Platzi!</h1>
-      <p>Bienvenidos al curso de Next.JS</p>
+      <p>Bienvenidos todos al curso de Next.Js</p>
+
+      <img src="/static/platzi-logo.png" alt="Platzi" />
+
+      <style jsx>{`
+        h1 {
+          color: red;
+        }
+        :global(div p) {
+          color: green;
+        }
+        img {
+          max-width: 50%;
+          display: block;
+          margin: 0 auto;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        body {
+          background: white;
+        }
+      `}</style>
     </div>
   }
 }
