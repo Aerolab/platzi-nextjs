4eac96955635b1204595c53c95467c20409b50ce
Roberto Gonzalez <roberto@aerolab.co>
2018-07-09 02:14:56 -0300

    5.Parametros

--- pages/channel.js
+++ pages/channel.js
@@ -1,10 +1,59 @@
+import Link from 'next/link'
+
 export default class extends React.Component {
 
+  static async getInitialProps({ query }) {
+    let idChannel = query.id
+
+    let [reqChannel, reqSeries, reqAudios] = await Promise.all([
+      fetch(`https://api.audioboom.com/channels/${idChannel}`),
+      fetch(`https://api.audioboom.com/channels/${idChannel}/child_channels`),
+      fetch(`https://api.audioboom.com/channels/${idChannel}/audio_clips`)
+    ])
+
+    let dataChannel = await reqChannel.json()
+    let channel = dataChannel.body.channel
+
+    let dataAudios = await reqAudios.json()
+    let audioClips = dataAudios.body.audio_clips
+
+    let dataSeries = await reqSeries.json()
+    let series = dataSeries.body.channels
+
+    return { channel, audioClips, series }
+  }
+
   render() {
+    const { channel, audioClips, series } = this.props
+
     return <div>
       <header>Podcasts</header>
 
-      <h1>Canal</h1>
+      <div className="banner" style={{ backgroundImage: `url(${channel.urls.banner_image.original})` }} />
+
+      <h1>{ channel.title }</h1>
+
+      { series.length > 0 &&
+        <div>
+          <h2>Series</h2>
+          <div className="channels">
+            { series.map((serie) => (
+              <Link href={`/channel?id=${ serie.id }`} prefetch>
+                <a className="channel">
+                  <img src={ serie.urls.logo_image.original } alt=""/>
+                  <h2>{ serie.title }</h2>
+                </a>
+              </Link>
+            ))}
+          </div>
+        </div>
+      }
+
+      <h2>Ultimos Podcasts</h2>
+      { audioClips.map((clip) => (
+        <div className="podcast" key={clip.id}>{ clip.title }</div>
+      ))}
+
 
       <style jsx>{`
         header {
@@ -13,6 +62,15 @@ export default class extends React.Component {
           padding: 15px;
           text-align: center;
         }
+
+        .banner {
+          width: 100%;
+          padding-bottom: 25%;
+          background-position: 50% 50%;
+          background-size: cover;
+          background-color: #aaa;
+        }
+
         .channels {
           display: grid;
           grid-gap: 15px;
@@ -30,12 +88,36 @@ export default class extends React.Component {
           box-shadow: 0px 2px 6px rgba(0,0,0,0.15);
           width: 100%;
         }
+        h1 {
+          font-weight: 600;
+          padding: 15px;
+        }
         h2 {
           padding: 5px;
           font-size: 0.9em;
           font-weight: 600;
           margin: 0;
           text-align: center;
+        }
+
+        .podcast {
+          display: block;
+          text-decoration: none;
+          color: #333;
+          padding: 15px;
+          border-bottom: 1px solid rgba(0,0,0,0.2);
+          cursor: pointer;
+        }
+        .podcast:hover {
+          color: #000;
+        }
+        .podcast h3 {
+          margin: 0;
+        }
+        .podcast .meta {
+          color: #666;
+          margin-top: 0.5em;
+          font-size: 0.8em;
         }
       `}</style>
 
