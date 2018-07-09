5982c61ce34d2affcb7cd197fb07468b4ddc99d4
Roberto Gonzalez <roberto@aerolab.co>
2018-07-09 03:09:44 -0300

    12.Loader

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,13 @@
 import { Link } from '../routes'
 import Head from 'next/head'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+Router.onRouteChangeStart = (url) => {
+  NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 export default class Layout extends React.Component {
   render() {
@@ -34,6 +42,81 @@ export default class Layout extends React.Component {
           font-family: system-ui;
           background: white;
         }
+
+        /* Make clicks pass-through */
+        #nprogress {
+          pointer-events: none;
+        }
+
+        #nprogress .bar {
+          background: #29d;
+
+          position: fixed;
+          z-index: 1031;
+          top: 0;
+          left: 0;
+
+          width: 100%;
+          height: 2px;
+        }
+
+        /* Fancy blur effect */
+        #nprogress .peg {
+          display: block;
+          position: absolute;
+          right: 0px;
+          width: 100px;
+          height: 100%;
+          box-shadow: 0 0 10px #29d, 0 0 5px #29d;
+          opacity: 1.0;
+
+          -webkit-transform: rotate(3deg) translate(0px, -4px);
+              -ms-transform: rotate(3deg) translate(0px, -4px);
+                  transform: rotate(3deg) translate(0px, -4px);
+        }
+
+        /* Remove these to get rid of the spinner */
+        #nprogress .spinner {
+          display: block;
+          position: fixed;
+          z-index: 1031;
+          top: 15px;
+          right: 15px;
+        }
+
+        #nprogress .spinner-icon {
+          width: 18px;
+          height: 18px;
+          box-sizing: border-box;
+
+          border: solid 2px transparent;
+          border-top-color: #29d;
+          border-left-color: #29d;
+          border-radius: 50%;
+
+          -webkit-animation: nprogress-spinner 400ms linear infinite;
+                  animation: nprogress-spinner 400ms linear infinite;
+        }
+
+        .nprogress-custom-parent {
+          overflow: hidden;
+          position: relative;
+        }
+
+        .nprogress-custom-parent #nprogress .spinner,
+        .nprogress-custom-parent #nprogress .bar {
+          position: absolute;
+        }
+
+        @-webkit-keyframes nprogress-spinner {
+          0%   { -webkit-transform: rotate(0deg); }
+          100% { -webkit-transform: rotate(360deg); }
+        }
+        @keyframes nprogress-spinner {
+          0%   { transform: rotate(0deg); }
+          100% { transform: rotate(360deg); }
+        }
+
       `}</style>
     </div>
   }
